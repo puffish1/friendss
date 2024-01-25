4f03846005644f8cf14a684a15f4ad936b0c9af8
puffish1 <116806424+puffish1@users.noreply.github.com>
2024-01-25 23:01:49 +0300

    Add files via upload

---
+++ main/script_main.js
@@ -0,0 +1,70 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function knopka() {
+    let ima = document.getElementById("res").value;
+    let ima1 = document.getElementById("ress").value;
+    let ima2 = document.getElementById("raes").value;
+    let ima_fio = ima + " " + ima1 + " " + ima2; 
+    document.getElementById("fio").innerHTML=ima_fio;
+
+    let fun = document.getElementById("email").value;
+    document.getElementById("mail").innerHTML=fun;
+
+    let jojo = document.getElementById("city").value;
+    document.getElementById("gorod").innerHTML=jojo;
+
+}
+
+function knopka(){
+    let ima = document.getElementById("res").value;
+   
+    if (ima == "admin") {
+        let ima1 = document.getElementById("ress").value;
+    let ima2 = document.getElementById("raes").value;
+    let ima_fio =  ima1 + " " + ima2; 
+    document.getElementById("fio").innerHTML=ima_fio;
+
+    let fun = document.getElementById("email").value;
+    document.getElementById("mail").innerHTML=fun;
+
+    let jojo = document.getElementById("city").value;
+    document.getElementById("gorod").innerHTML=jojo;
+
+    }
+
+    else {
+        let no = "POKA"
+    document.getElementById("fio").innerHTML=no;
+        
+    }
+}*/
+
+
+
+
